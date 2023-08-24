a326672b490ccc50f21b8f6675bf6efff8106d03
BuildTools <53991956+MugungHa@users.noreply.github.com>
2023-08-24 20:36:02 +0900

    몬스터 멈춤 현상 수정

--- Assets/스크립트/몬스터 스크립트/MonsterHealth.cs
+++ Assets/스크립트/몬스터 스크립트/MonsterHealth.cs
@@ -3,39 +3,41 @@ using UnityEngine.UI;
 
 public class MonsterHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentHealth;
-    private Animator animator;
+	public int maxHealth = 100;
+	public int currentHealth;
+	private Animator animator;
+	private MonsterMovement monsterMovement; // 개별 몬스터의 이동 상태를 저장하는 변수
 
-    private void Start()
-    {
-        currentHealth = maxHealth;
-        animator = GetComponent<Animator>();
-    }
+	private void Start()
+	{
+		currentHealth = maxHealth;
+		animator = GetComponent<Animator>();
+		monsterMovement = GetComponent<MonsterMovement>(); // MonsterMovement 스크립트 가져오기
+	}
 
-    public void TakeDamage(int damage)
-    {
-        MonsterMovement monsterMovement = FindObjectOfType<MonsterMovement>();
-        currentHealth -= damage;
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-        else
-        {
-            animator.SetTrigger("TakeDamage");
-            monsterMovement.StopMovement();
-            Debug.Log("체력이 감소했습니다");// 체력이 감소할 때마다 체력바 및 텍스트 업데이트
-        }
-    }
+	public void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+		else
+		{
+			animator.SetTrigger("TakeDamage");
+			monsterMovement.StopMovement(); // 해당 몬스터의 이동 중지
+			Debug.Log("체력이 감소했습니다"); // 체력이 감소할 때마다 체력바 및 텍스트 업데이트
+		}
+	}
 
-    private void Die()
-    {
-        MonsterPortal monsterPortal = FindObjectOfType<MonsterPortal>();
-        if (monsterPortal != null)
-        {
-            monsterPortal.MonsterKilled();
-        }
-        Destroy(gameObject);
-    }
+	private void Die()
+	{
+		MonsterPortal monsterPortal = FindObjectOfType<MonsterPortal>();
+		if (monsterPortal != null)
+		{
+			monsterPortal.MonsterKilled();
+		}
+		monsterMovement.ResumeMovement(); // 해당 몬스터의 이동 재개
+		Destroy(gameObject);
+	}
 }
--- Assets/스크립트/몬스터 스크립트/MonsterMovement.cs
+++ Assets/스크립트/몬스터 스크립트/MonsterMovement.cs
@@ -4,39 +4,60 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
-    public float moveSpeed = 3.0f;
-    public string targetTag = "Castle"; // 타겟 오브젝트의 태그
+	public float moveSpeed = 3.0f;
+	public string targetTag = "Castle"; // 타겟 오브젝트의 태그
 
-    private Transform target; // 이동할 목표 위치
-    private bool isMoving = true; // 이동 가능 상태를 나타내는 변수
-    private Animator animator;
+	private Transform target; // 이동할 목표 위치
+	private bool isMoving = true; // 이동 가능 상태를 나타내는 변수
+	private Animator animator;
 
-    private void Start()
-    {
-        // 태그로 타겟을 찾아 할당합니다.
-        target = GameObject.FindGameObjectWithTag(targetTag)?.transform;
-        animator = GetComponent<Animator>();
-    }
+	// 개별 몬스터의 이동 상태를 저장하는 리스트
+	private static List<MonsterMovement> monsters = new List<MonsterMovement>();
 
-    private void Update()
-    {
-        if (target != null && isMoving)
-        {
-            // 몬스터가 목표 위치로 이동합니다.
-            Vector3 direction = target.position - transform.position;
-            direction.y = 0; // 수직 이동 방지
-            transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
-        }
-        animator.SetBool("IsMoving", isMoving);
-    }
+	private void Start()
+	{
+		// 태그로 타겟을 찾아 할당합니다.
+		target = GameObject.FindGameObjectWithTag(targetTag)?.transform;
+		animator = GetComponent<Animator>();
 
-    public void StopMovement()
-    {
-        isMoving = false; // 이동 중지
-    }
+		// 리스트에 현재 몬스터를 추가합니다.
+		monsters.Add(this);
+	}
 
-    public void ResumeMovement()
-    {
-        isMoving = true; // 이동 재개
-    }
+	private void Update()
+	{
+		if (target != null && isMoving)
+		{
+			// 몬스터가 목표 위치로 이동합니다.
+			Vector3 direction = target.position - transform.position;
+			direction.y = 0; // 수직 이동 방지
+			transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
+		}
+		animator.SetBool("IsMoving", isMoving);
+	}
+
+	public void StopMovement()
+	{
+		isMoving = false; // 이동 중지
+	}
+
+	public void ResumeMovement()
+	{
+		isMoving = true; // 이동 재개
+	}
+
+	// 모든 몬스터에 대해 이동을 재개하도록 하는 함수
+	public static void ResumeAllMonsters()
+	{
+		foreach (var monster in monsters)
+		{
+			monster.ResumeMovement();
+		}
+	}
+
+	// 개별 몬스터 제거 시 리스트에서 제거
+	private void OnDestroy()
+	{
+		monsters.Remove(this);
+	}
 }
