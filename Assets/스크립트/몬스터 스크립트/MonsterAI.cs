c839c9d469cfc5c814dda11041c9804fec507e20
PPogPPogs <dhwjdals2874@naver.com>
2023-10-06 20:00:25 +0900

    제발~

--- Assets/스크립트/몬스터 스크립트/MonsterAI.cs
+++ Assets/스크립트/몬스터 스크립트/MonsterAI.cs
@@ -7,11 +7,13 @@ public class MonsterAI : MonoBehaviour
     public float detectionRange = 10.0f; // 플레이어 감지 범위
     private Transform player; // 플레이어의 Transform
     private MonsterAttack monsterAttack;
+    private MonsterMovement monsterMovement;  
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         monsterAttack = GetComponent<MonsterAttack>();
+        monsterMovement = GetComponent<MonsterMovement>();
     }
 
     private void Update()
@@ -20,8 +22,13 @@ public class MonsterAI : MonoBehaviour
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
         if (distanceToPlayer <= detectionRange)
         {
-            // 플레이어를 감지하면 공격
+            monsterMovement.StopMovement();
             monsterAttack.AttackPlayer();
+        }
+        else if(distanceToPlayer > detectionRange)
+        {
+
+            monsterMovement.ResumeMovement();
         }
     }
 }
--- Assets/스크립트/몬스터 스크립트/MonsterAttack.cs
+++ Assets/스크립트/몬스터 스크립트/MonsterAttack.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterAttack : MonoBehaviour
@@ -7,13 +5,15 @@ public class MonsterAttack : MonoBehaviour
     public float attackRange = 2.0f;
     public float attackCooldown = 2.0f;
     public int damageAmount = 10; // 몬스터의 공격 데미지
-
+    private Animator animator;
+    
     private float nextAttackTime = 0.0f;
 
     private CharacterHealth playerHealth; // 플레이어의 CharacterHealth 스크립트를 연결
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         // "Player" 태그를 가진 GameObject를 찾아 CharacterHealth 스크립트를 연결
         GameObject player = GameObject.FindWithTag("Player");
         if (player != null)
@@ -35,6 +35,9 @@ public class MonsterAttack : MonoBehaviour
 
             // 플레이어의 체력을 감소시킴
             playerHealth.TakeDamage(damageAmount);
+
+            // 공격 애니메이션을 시작합니다.
+            animator.SetTrigger("Attack"); // "Attack"라는 트리거를 설정하여 공격 애니메이션을 재생합니다.
 
             // 공격 후 쿨다운 타임 설정
             nextAttackTime = Time.time + attackCooldown;
