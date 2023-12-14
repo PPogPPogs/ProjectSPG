997ac1fd0f8d941ec228cf6714315e2a0b17200d
BuildTools <53991956+MugungHa@users.noreply.github.com>
2023-12-15 03:51:54 +0900

    으흠

---
+++ Assets/에셋이요/배경/이미지/건물/이걸 본다면 디코로 염뽕삐리봉이라고 보내보거라.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 이걸본다면디코로염뽕삐리봉을 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
